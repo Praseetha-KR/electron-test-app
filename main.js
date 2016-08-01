46c03a1af9a0053de7ea361aa48c077cd9e40a00
Praseetha-KR <voidimagineer@yahoo.com>
2016-08-02 01:14:10 +0530

    Corrected hello world script

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow} = require('electron')
 
-let win;
+let win
 
 function createWindow() {
-    win = new BrowserWindow({width: 800, height: 600});
-    win.loadURL(`file://${__dirname}/index.html`);
-    win.webContents.openDevTools();
+    win = new BrowserWindow({width: 800, height: 600})
+    win.loadURL(`file://${__dirname}/index.html`)
+    win.webContents.openDevTools()
     win.on('closed', () => {
-        win = null;
-    });
-
-    app.on('ready', createWindow);
-    app.on('window-all-closed', () => {
-        if(process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
-    app.on('activate', () => {
-        if (win == null) {
-            createWindow();
-        }
-    });
+        win = null
+    })
 }
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', () => {
+    if (win == null) {
+        createWindow()
+    }
+})
