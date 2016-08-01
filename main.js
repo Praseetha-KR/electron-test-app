bba1f67189ff8692e91a6436868e281049842e05
Praseetha-KR <voidimagineer@yahoo.com>
2016-08-02 02:10:33 +0530

    Add modal window with resize/move log

--- main.js
+++ main.js
@@ -1,38 +1,24 @@
 const {app, BrowserWindow} = require('electron')
 
-const manageWindowBtn = document.getElementById('manage-window')
+let win
 
-manageWindowBtn.addEventListener('click', function(event) {
-    let win = new BrowserWindow({width: 400, height: 275})
-    win.on('resize', updateReply)
-    win.on('move', updateReply)
-    win.on('close', function() { win = null })
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600})
     win.loadURL(`file://${__dirname}/index.html`)
-    win.show()
-})
-
-function updateReply() {
-    const manageWindowReply = document.getElementById('manage-window-reply')
-    const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
-
-    manageWindowReply.innerText = message
+    win.webContents.openDevTools()
+    win.on('close', () => {
+        win = null
+    })
 }
 
-// function createWindow() {
-//     win = new BrowserWindow({width: 800, height: 600})
-//     win.loadURL(`file://${__dirname}/index.html`)
-//     win.webContents.openDevTools()
-//     win.on('close', () => win = null)
-// }
-
-// app.on('ready', createWindow)
-// app.on('window-all-closed', () => {
-//     if(process.platform !== 'darwin') {
-//         app.quit()
-//     }
-// })
-// app.on('activate', () => {
-//     if (win == null) {
-//         createWindow()
-//     }
-// })
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', () => {
+    if (win == null) {
+        createWindow()
+    }
+})
